ac33c9dd45811e1078b9f74d65ee4bfafad9c4fa
Andrew Zimmerman <astevezimm@gmail.com>
2022-07-03 21:57:05 -0600

    add my games to the unity page

--- public/js/content-offset.js
+++ public/js/content-offset.js
@@ -1,4 +1,9 @@
-﻿const offset = () => $(".content").css("margin-top", $(".header").height());
+﻿function offset() {
+    $(".content").css("margin-top", $(".header").height());
+    const iframe = $("iframe");
+    if (iframe)
+        iframe.height(iframe.width() * 0.625);
+}
 offset();
 $(window).resize(offset);
 $(".navbar-toggler").click(() => setTimeout(offset, 300));
