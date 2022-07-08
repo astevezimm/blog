328d9be8db21c2f19d47a2155abf93716d4b6135
Andrew Zimmerman <astevezimm@gmail.com>
2022-07-07 21:01:21 -0600

    Blog page render actual content, stagger colors, other css tweaks for blog page

--- public/js/content-offset.js
+++
@@ -1,9 +0,0 @@
-﻿function offset() {
-    $(".content").css("margin-top", $(".header").height());
-    const iframe = $("iframe");
-    if (iframe)
-        iframe.height(iframe.width() * 0.625);
-}
-offset();
-$(window).resize(offset);
-$(".navbar-toggler").click(() => setTimeout(offset, 300));
---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+﻿function offset() {
+    $(".content").css("margin-top", $(".header").height());
+    const iframe = $("iframe");
+    if (iframe)
+        iframe.height(iframe.width() * 0.625);
+}
+offset();
+$(window).resize(offset);
+$(".navbar-toggler").click(() => setTimeout(offset, 300));
+
+function styleEvenPosts() {
+    const posts = $(".post");
+    if (posts) {
+        const size = window.innerWidth;
+        console.log(size);
+        const postsPerRow = size >= 1200 ? 4 : size >= 992 ? 3 : size >= 768 ? 2 : 1;
+        for (let post = 0, row = 0; post < posts.length; row++)
+            for (let col = 0; post < posts.length && col < postsPerRow; col++, post++) {
+                if (row % 2 && !(col % 2) || !(row % 2) && col % 2)
+                    posts[post].classList.add("post-even");
+                else
+                    posts[post].classList.remove("post-even");
+            }
+    }
+}
+styleEvenPosts();
+$(window).resize(styleEvenPosts);
